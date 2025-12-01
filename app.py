52a9afe6193151269bc39b71912ef814de769e13
fijaz <8086fijazfbz@gmail.com>
2025-12-02 00:32:04 +0530

    fix docker 7

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "guuuuuujaaaaaaa potte ....poyi thungada thevdiya........"
+    return "Hello from Flask running in Docker! "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
